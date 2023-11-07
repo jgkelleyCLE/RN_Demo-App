be8deb087b62c52ae163f26ddbe4e6b32b9af68a
Jack <74841896+jgkelleyCLE@users.noreply.github.com>
2023-11-07 17:03:40 -0500

    Updated text in App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View className="bg-blue-500 flex-1 items-center justify-center">
-      <Text>First commit!</Text>
+      <Text>Change text from Github</Text>
       <StatusBar style="auto" />
     </View>
   );
